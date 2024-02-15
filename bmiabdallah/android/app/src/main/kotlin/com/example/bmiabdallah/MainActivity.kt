38f3a30780864c4c099806160b1367d0082a4ec9
Mahmoud Shoaib <sim.MahmoudShoaib23180@alexu.edu.eg>
2024-02-15 13:25:49 +0200

    feature

---
+++ bmiabdallah/android/app/src/main/kotlin/com/example/bmiabdallah/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bmiabdallah
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ bmiabdallah/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:bmiabdallah/bmi_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  runApp( MyApp(),
+
+
+  );
+}
+
+class MyApp extends StatelessWidget {
+
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+        debugShowCheckedModeBanner: false,
+        home:BmiScreen(),
+    );
+  }
+}
+
